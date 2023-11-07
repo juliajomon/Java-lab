f1a2981edba1738a48cf3c0fa58d42e0bca51334
Julia Jomon <juliajomon12@gmail.com>
2023-11-07 15:58:15 +0530

    Update Employeee.java

--- Employeee.java
+++ Employeee.java
@@ -5,49 +5,42 @@ class Employee{
 	int PhoneNo ;
 	String Address;
 	int Salary;
-		
-	 void DisplayEmployeeInfo(){
-		System.out.println("Name: "+Name);
-		System.out.println("Age: "+Age);
-		System.out.println("Phone Number: "+PhoneNo);
-		System.out.println("Address: "+Address);
-		System.out.println("Salary: " + Salary );
-	}
-}
-
-class Officer extends Employee{
-	String Specialization;
-	//constructer officer
-	Officer(String name ,int age, int phoneNo , String address , int salary , String specialization){
+	
+	Employee(String name ,int age, int phoneNo , String address , int salary){
 		Name = name;
 		Age = age;
 		PhoneNo = phoneNo;
 		Address = address;
 		Salary = salary;
+	}
+		
+	public String toString(){
+		return "Name: "+Name + "\nAge: "+Age + "\nPhone Number: "+PhoneNo + "\nAddress: "+Address + "\nSalary: " + Salary ;
+	}
+}
+class Officer extends Employee{
+	String Specialization;
+	//constructer officer
+	Officer(String name ,int age, int phoneNo , String address , int salary, String specialization){
+		super(name,age,phoneNo,address,salary);
 		Specialization = specialization;
 	}
-	void DisplayEmployeeInfo(){
-		super.DisplayEmployeeInfo();
-		System.out.println("Specialization: " + Specialization );
-		System.out.println("\n");
+	
+	public String toString(){
+		return "Name: "+Name + "\nAge: "+Age + "\nPhone Number: "+PhoneNo + "\nAddress: "+Address + "\nSalary: " + Salary +"\nSpecialization: " + Specialization;
 	}
 }
 
 class Manager extends Employee{
 	String Department;
 	//constructer manager
-	Manager(String name , int age , int phoneNo , String address , int salary, String department){
-		Name = name;
-		Age = age;
-		PhoneNo = phoneNo;
-		Address = address;
-		Salary = salary;
+	Manager(String name ,int age, int phoneNo , String address , int salary, String department){
+		super(name,age,phoneNo,address,salary);
 		Department = department;
 	}
-	void DisplayEmployeeInfo(){
-		super.DisplayEmployeeInfo();
-		System.out.println("Department: " + Department );
-		System.out.println("\n");
+	
+	public String toString(){
+		return "Name: "+Name + "\nAge: "+Age + "\nPhone Number: "+PhoneNo + "\nAddress: "+Address + "\nSalary: " + Salary 			+ 	"\nDepartment: " + Department;
 	}
 }
 
@@ -55,8 +48,8 @@ class Employeee{
 	public static void main(String[] args){
 		Officer D1= new Officer("julia",34,9372482,"ABC valley",9875,"AR");
 		Manager M1 = new Manager("jomon",38,9372482,"DEF valley",9465,"CS");
-		D1.DisplayEmployeeInfo();
-		M1.DisplayEmployeeInfo();
+		System.out.println(D1.toString());
+		System.out.println(M1.toString());
 		
 	}
 }
