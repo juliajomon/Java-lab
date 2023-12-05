78e419b88cb6235520e7c82790f883cb5a0e6c59
Julia Jomon <juliajomon12@gmail.com>
2023-12-05 16:29:43 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,19 +1,18 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.*;
 
-class cal implements ActionListener
-{
-    JFrame f;
-    JTextField t;
-
-    JButton b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,b0,bdiv,bmul,bsub,badd,beq,bclr,bdec;
-    static double a=0,b=0,result = 0;
-    static int operator = 0;
-    public cal()
-    {
-        f = new JFrame("CALCULATOR");
-        t = new JTextField();
-        b1 = new JButton("1");
+class Calculator  
+{	
+	  double a=0,b=0,result = 0;
+      char opp;
+	
+	Calculator(){
+		JFrame f = new JFrame("CALCULATOR");
+		JTextField t= new JTextField(10);
+	    JButton b1,b2,b3,b4,b5,b6,b7,b8,b9,b0,bdiv,bmul,bsub,badd,beq,bclr,bdec;
+	   
+		b1 = new JButton("1");
         b2 = new JButton("2");
         b3 = new JButton("3");
         b4 = new JButton("4");
@@ -23,100 +22,223 @@ class cal implements ActionListener
         b8 = new JButton("8");
         b9 = new JButton("9");
         b0 = new JButton("0");
-        bmul = new JButton("x");
+        bmul = new JButton("*");
         bdiv = new JButton("/");
         badd = new JButton("+");
         bsub = new JButton("-");
         beq = new JButton("=");
-        bdec = new JButton("1");
+        bdec = new JButton(".");
         bclr = new JButton("clear");
+        
+		f.setLayout(new GridLayout(6,1));
+		
+		JPanel p1 = new JPanel();
+		p1.add(t);
+		
+		JPanel p2 = new JPanel();
+		p2.setLayout(new GridLayout(1,4));
+		p2.add(b1);
+        p2.add(b2);
+        p2.add(b3);
+        p2.add(badd);
+		
+		JPanel p3 = new JPanel();
+		p3.setLayout(new GridLayout(1,4));
+        p3.add(b4);
+        p3.add(b5);
+        p3.add(b6);
+        p3.add(bsub);
 
-        t.setBounds(30,40,280,30);
-
-        b1.setBounds(40,100,50,40);
-        b2.setBounds(110,100,50,40);
-        b3.setBounds(180,100,50,40);
-        badd.setBounds(250,100,50,40);
-
-        b4.setBounds(40,170,50,40);
-        b5.setBounds(110,170,50,40);
-        b6.setBounds(180,170,50,40);
-        bsub.setBounds(250,170,50,40);
-
-        b7.setBounds(40,240,50,40);
-        b8.setBounds(110,240,50,40);
-        b9.setBounds(180,240,50,40);
-        bdiv.setBounds(250,240,50,40);
-
-        bdec.setBounds(40,310,50,40);
-        b0.setBounds(110,310,50,40);
-        beq.setBounds(180,310,50,40);
-        bmul.setBounds(250,310,50,40);
-
-        bclr.setBounds(40,380,200,40);
-
-        f.add(t);
-
-        f.add(b1);
-        f.add(b2);
-        /*f.add(t);
-        f.add(t);
-
-        f.add(t);
-        f.add(t);
-        f.add(t);
-        f.add(t);
-
-        f.add(t);
-        f.add(t);
-        f.add(t);
-        f.add(t);*/
-
-        f.setLayout(null);
-        f.setVisible(true);
-        f.setSize(350,500);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setResizable(false);
-
-        b1.addActionListener(this);//add to all button
-
-
-    }
-    public void actionPerformed(ActionEvent e)
-    {
-        if(e.getSource() == b1)
-            t.setText(t.getText().concat("9"));//apply to all
-
-        if(e.getSource() == badd)
-        {   a=Double.parseDouble(t.getText());
-            operator=1;
-            t.setText("");
-        }//add to opp and increament operator
-        if(e.getSource() == beq){
-            b=Double.parseDouble(t.getText());
-            switch (operator) {
-                case 1:
-                    result = a+b;
-                    break;//add all case
-
-            
-                default: result=0;
+		JPanel p4 = new JPanel();
+		p4.setLayout(new GridLayout(1,4));
+        p4.add(b7);
+        p4.add(b8);
+        p4.add(b9);
+        p4.add(bdiv);
+        
+        JPanel p5 = new JPanel();
+		p5.setLayout(new GridLayout(1,4));
+        p5.add(bdec);
+        p5.add(b0);
+        p5.add(beq);
+        p5.add(bmul);
+        
+        JPanel p6 = new JPanel();
+        p6.add(bclr);
+		
+		f.add(p1);
+		f.add(p2);
+		f.add(p3);
+		f.add(p4);
+		f.add(p5);
+		f.add(p6);
+		
+		b0.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e){
+		    t.setText(t.getText().concat("0"));
+		    
+		    }
+		});  
+		
+		b1.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e){
+		    t.setText(t.getText().concat("1"));
+		    }
+		});
+		
+		b2.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e){
+		    t.setText(t.getText().concat("2"));
+		    }
+		});
+		
+		b3.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e){
+		    t.setText(t.getText().concat("3"));
+		    }
+		});
+		
+		b4.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e){
+		    t.setText(t.getText().concat("4"));
+		    }
+		});
+		
+		b5.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e){
+		    t.setText(t.getText().concat("5"));
+		    }
+		});
+		
+		b6.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e){
+		    t.setText(t.getText().concat("6"));
+		    }
+		});
+		
+		b7.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e){
+		    t.setText(t.getText().concat("7"));
+		    }
+		});
+		
+		b8.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e){
+		    t.setText(t.getText().concat("8"));
+		    }
+		});
+		
+		b9.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e){
+		    t.setText(t.getText().concat("9"));
+		    }
+		});
+		
+		bdec.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e){
+		    t.setText(t.getText().concat("."));
+		    }
+		});
+		
+		badd.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e){
+		    a=Double.parseDouble(t.getText());
+		    opp = '+';
+		    t.setText("");
+		    
+		    }
+		});
+		
+		bsub.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e){
+		    a=Double.parseDouble(t.getText());
+		    opp = '-';
+		    t.setText("");
+		    
+		    }
+		});
+		
+		bmul.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e){
+		    a=Double.parseDouble(t.getText());
+		    opp = '*';
+		    t.setText("");
+		    }
+		});
+		
+		bdiv.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e){
+		    a=Double.parseDouble(t.getText());
+		    opp = '/';
+		    t.setText("");
+		    }
+		});
+	
+		beq.addActionListener(new ActionListener()
+		{	
+			public void actionPerformed(ActionEvent e){
+			b=Double.parseDouble(t.getText());
+			
+		    switch(opp){
+		    	case '+':
+ 					result =a+b;
+		    		break;
+		    		
+		    	case '-':
+ 					result =a-b;
+		    		break;
+		    		
+		    	case '*':
+ 					result =a*b;
+		    		break;
+		    		
+		    	case '/':
+ 					result =a/b;
+		    		break;
+		    		
+		    	default: result=0;
                     break;
-            }
-            t.setText(""+result);
-        }
-        if(e.getSource() == bclr){
-            t.setText("");
-        }
-
-    }
-}
-/**
- * Calculator
- */
-public class Calculator {
-
+		    		
+		    }
+		     t.setText("="+result);
+		   }
+		});
+		bclr.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e){
+		     t.setText(""+result);
+		    }
+		});
+		
+		
+		f.setVisible(true);
+		f.setSize(350,500);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+	}
     public static void main(String[] args) {
-        new cal();
+        new Calculator();
     }
 }
+
+
+
+
+
+
+
